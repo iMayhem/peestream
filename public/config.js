7813007c1164eaaf29998dbc6793c8496b789245
iMayhem <imayhem@users.noreply.github.com>
2026-07-06 08:52:27 +0000

    update URLs to https://peestream.in domain

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://proxy.peestream.in",
 
-  VITE_BACKEND_URL: "https://server.vidbinge.com",
+  VITE_BACKEND_URL: "https://api.peestream.in",
 
   // The READ API key to access TMDB
   VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmE0YzJjN2MxZGUxMDA1YWRlZTgyNGRjNTU5MzY3MiIsIm5iZiI6MTc1NTM1ODg0NS45NCwic3ViIjoiNjhhMGE2N2RjMDM0MmI4ZmRkZWQxMzljIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.XQoZX0eJUCboSHFR15I1kZhXgIrKavsEamnKwusjj8I",
@@ -13,9 +13,6 @@ window.__CONFIG__ = {
 
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: true,
-
-  // The backend URL to communicate with
-  VITE_BACKEND_URL: null,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: ""
