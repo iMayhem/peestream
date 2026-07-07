bf62630c926755ca6673883117b5b7960e25809e
iMayhem <imayhem@users.noreply.github.com>
2026-07-07 05:47:54 +0000

    config: add server-side provider API url to VITE_CORS_PROXY_URL

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://proxy.peestream.in",
+  VITE_CORS_PROXY_URL: "https://proxy.peestream.in,|type=api|https://providers.peestream.in",
 
   VITE_BACKEND_URL: "https://api.peestream.in",
 
